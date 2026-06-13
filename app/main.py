64e4aaab9902bf6192578d24e39d5b6cbe83cf96
Pedro Burini <pulisses10@gmail.com>
2026-06-12 21:33:12 -0300

    fix: adiciona CORS middleware

--- app/main.py
+++ app/main.py
@@ -1,10 +1,18 @@
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 from app.routes import router
 
 app = FastAPI(
     title="API de Filmes e Séries",
     description="Agregador de informações e notas de filmes e séries usando TMDB e OMDb",
     version="1.0.0"
+)
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],
+    allow_methods=["*"],
+    allow_headers=["*"],
 )
 
 app.include_router(router, prefix="/api")
